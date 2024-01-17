16ab1045e3d99d1a7b13c6b2e05d1957e3611359
Sander Vispoel <sandervispoel@hotmail.com>
2024-01-17 22:32:10 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'sonner';
 
-import { cn } from 'services/cn';
 import { SizeIndicator } from 'common/size-indicator';
 
 import { Providers } from './Providers';
@@ -29,7 +28,7 @@ const RootLayout: React.FC<Props> = ({ children }) => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😃</text></svg>"
         />
       </head>
-      <body className={cn('h-full min-h-full', inter.className)}>
+      <body className={`h-full min-h-full ${inter.className}`}>
         <Providers>
           {children}
           <Toaster />
