239cecf19e9ff628be9046964cdb5485353be72f
Sander Vispoel <sandervispoel@hotmail.com>
2025-05-16 16:21:11 +0200

    Add copy tooltip

--- src/components/modules/home/emoji-button.tsx
+++ src/components/modules/home/emoji-button.tsx
@@ -3,24 +3,36 @@
 import * as React from 'react';
 import { toast } from 'sonner';
 
+import { Button, OverlayArrow, Tooltip, TooltipTrigger } from 'common/react-aria-components';
+
 type Props = {
   emoji: string;
 };
 
 export const EmojiButton: React.FC<Props> = (props) => {
   return (
-    <button
-      type="button"
-      className="aspect-square rounded-md p-2 transition-colors hover:bg-gray-200 dark:hover:bg-gray-700"
-      onClick={() => {
-        toast.promise(navigator.clipboard.writeText(props.emoji), {
-          success: 'Copied to clipboard!',
-          error: 'Failed to copy to clipboard',
-          loading: 'Copying to clipboard...',
-        });
-      }}
-    >
-      {props.emoji}
-    </button>
+    <TooltipTrigger delay={300} closeDelay={200}>
+      <Button
+        type="button"
+        className="aspect-square rounded-md p-2 transition-colors hover:bg-gray-200 dark:hover:bg-gray-700"
+        onClick={() => {
+          toast.promise(navigator.clipboard.writeText(props.emoji), {
+            success: 'Copied to clipboard!',
+            error: 'Failed to copy to clipboard',
+            loading: 'Copying to clipboard...',
+          });
+        }}
+      >
+        {props.emoji}
+      </Button>
+      <Tooltip className="rounded-md bg-black p-1 px-2 text-xs text-gray-100 shadow-sm dark:bg-gray-100 dark:text-gray-800">
+        <OverlayArrow className="fill-black dark:fill-gray-100">
+          <svg width={8} height={8} viewBox="0 0 8 8">
+            <path d="M0 0 L4 4 L8 0" />
+          </svg>
+        </OverlayArrow>
+        Click to copy
+      </Tooltip>
+    </TooltipTrigger>
   );
 };
